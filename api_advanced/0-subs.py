30bc669a71621d0c5d10b7fa641a1a9b9e070aa8
denyseishimirwe <d.ishimirwe@alustudent.com>
2025-07-24 12:51:32 +0000

    Add function to get number of subscribers from Reddit API

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,37 +1,21 @@
- #!/usr/bin/python3
+#!/usr/bin/python3
+"""
+Function to query Reddit API and return number of subscribers for a subreddit.
+"""
 
-import requests  # Import requests to make HTTP calls
+import requests
 
 def number_of_subscribers(subreddit):
     """
     Returns the number of subscribers for a given subreddit.
-    If the subreddit is invalid or an error occurs, returns 0.
+    If the subreddit is invalid or any error occurs, returns 0.
     """
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {"User-Agent": "custom-user-agent/1.0"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
 
-    # 1. Build the URL for the subreddit's about.json endpoint
-    url = f'https://www.reddit.com/r/{subreddit}/about.json'
-
-    # 2. Set a custom User-Agent to avoid getting blocked
-    headers = {'User-Agent': 'MyRedditApp/0.0.1'}
-
-    try:
-        # 3. Make the GET request with headers and disable redirects
-        response = requests.get(url, headers=headers, allow_redirects=False)
-
-        # 4. If status code is not 200 (success), return 0
-        if response.status_code != 200:
-            return 0
-
-        # 5. Parse the JSON response into a Python dictionary
+    if response.status_code == 200:
         data = response.json()
-
-        # 6. Try to get the number of subscribers from the JSON
-        subscribers = data.get('data', {}).get('subscribers', 0)
-
-        # 7. Return the number of subscribers
-        return subscribers
-
-    except requests.RequestException:
-        # 8. If something goes wrong with the request, return 0
+        return data.get("data", {}).get("subscribers", 0)
+    else:
         return 0
-
