1a8fbdb65cc4982148c0655d332b13bbf49e9b83
denyseishimirwe <d.ishimirwe@alustudent.com>
2025-08-04 09:33:40 +0000

    permission

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,46 +1,24 @@
 #!/usr/bin/python3
-"""
-Module for querying the Reddit API to get the titles of the first 10
-hot posts for a given subreddit.
-"""
-
+'''
+    this module contains the function top_ten
+'''
 import requests
+from sys import argv
 
 
 def top_ten(subreddit):
-    """
-    Queries the Reddit API and prints the titles of the first 10 hot posts
-    listed for a given subreddit.
-
-    Args:
-        subreddit (str): The name of the subreddit.
-    """
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
-    headers = {
-        "User-Agent": "MyCustomRedditApp/1.0"  
-    }
-
+    '''
+        returns the top ten posts for a given subreddit
+    '''
+    user = {'User-Agent': 'Lizzie'}
+    url = requests.get('https://www.reddit.com/r/{}/hot/.json?limit=10'
+                       .format(subreddit), headers=user).json()
     try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-
-        if response.status_code == 200:
-            data = response.json()
-            posts = data.get("data", {}).get("children", [])
-
-            if not posts:
-                print(None) 
-                return
-
-            for post in posts:
-                title = post.get("data", {}).get("title")
-                if title:
-                    print(title)
-        elif response.status_code == 404 or response.is_redirect:
-            
-            print(None)
-        else:
-            print(None)
-    except requests.exceptions.RequestException:
+        for post in url.get('data').get('children'):
+            print(post.get('data').get('title'))
+    except Exception:
         print(None)
-    except ValueError: 
-        print(None)
+
+
+if __name__ == "__main__":
+    top_ten(argv[1])
